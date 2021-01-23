6faddb7f29d4518f4c7c98b391cc5acb38d20743
NormalZHC <zouhaochenluke@gmail.com>
2021-01-24 05:58:14 +0800

    Commit for the first time by Haochen Zou

---
+++ src/main/java/team05/demo01/HelloWorld.java
@@ -0,0 +1,8 @@
+package team05.demo01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Haochen Zou");
+    }
+}
