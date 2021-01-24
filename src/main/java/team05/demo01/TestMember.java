cc411d13e5cf047c553cd5676f58022f2751850c
DeJian Wang <wangdejianwang@gmail.com>
2021-01-24 15:41:32 -0500

    DJW env setup ready, 2021/01/24

--- src/main/java/team05/demo01/TestMember.java
+++ src/main/java/team05/demo01/TestMember.java
@@ -4,5 +4,7 @@ public class TestMember {
     public static void main(String[] args) {
         TeamMember t1=new TeamMember("Haochen Zou","zouhaochenluke@gmail.com");
         System.out.println(t1.getName()+','+t1.getEmail());
+        TeamMember t2=new TeamMember("Dejian Wang","wangdejianwang@gmail.com");
+        System.out.println(t2.getName()+','+t2.getEmail());
     }
 }
