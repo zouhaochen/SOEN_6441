5954a31fd876003a6cb5c43ec9fcbf281fa5bb31
luyongtang <wilsonluyongtang_2005@hotmail.com>
2021-01-24 18:06:58 -0500

    Complete local env setup

--- src/main/java/team05/demo01/TestMember.java
+++ src/main/java/team05/demo01/TestMember.java
@@ -6,5 +6,7 @@ public class TestMember {
         System.out.println(t1.getName()+','+t1.getEmail());
         TeamMember t2=new TeamMember("Dejian Wang","wangdejianwang@gmail.com");
         System.out.println(t2.getName()+','+t2.getEmail());
+        TeamMember t3=new TeamMember("Yongtang Lu","wilsonluyongtang_2005@hotmail.com");
+        System.out.println(t3.getName()+','+t3.getEmail());
     }
 }
