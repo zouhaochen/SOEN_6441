ae8ea1464f7f14ce298c0c43dd7d9d4f3c5c5f88
WangZT01 <18640096737@163.com>
2021-01-28 08:40:49 +0800

    添加个人信息

--- src/main/java/team05/demo01/TestMember.java
+++ src/main/java/team05/demo01/TestMember.java
@@ -10,5 +10,7 @@ public class TestMember {
         System.out.println(t3.getName()+','+t3.getEmail());
         TeamMember t4=new TeamMember("Yongtang Lu","wilsonluyongtang_2005@hotmail.com");
         System.out.println(t4.getName()+','+t4.getEmail());
+        TeamMember t5=new TeamMember("Zitao Wang","wangzitao9@gmail.com");
+        System.out.println(t4.getName()+','+t4.getEmail());
     }
 }
