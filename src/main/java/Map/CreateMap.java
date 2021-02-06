8b2aa0495493e1cda2c281027c599d1763769386
WangZT01 <18640096737@163.com>
2021-02-07 00:56:13 +0800

    创建地图文件的Junit简单测试。无关联任何其他类，可删除。

---
+++ src/main/java/Map/CreateMap.java
@@ -0,0 +1,19 @@
+package Map;
+import java.util.Scanner;
+
+/**
+ * This class is to Divide Army and Country to the Players accordingly.
+ *
+ * @author Zitao Wang
+ * @version 1.0.0
+ * */
+
+public class CreateMap {
+
+    public void createMap(){
+
+        System.out.println("地图创建");
+
+    }
+
+}
---
+++ src/test/java/MapTest/CreateMapTest.java
@@ -0,0 +1,22 @@
+package MapTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * This class is for testing the function of creating map
+ * @author Zitao Wang
+ * @version 1.0.0
+ */
+
+public class CreateMapTest {
+    @Test
+    /**
+     * This method is test case for ColorSelectTest
+     */
+    public void test(){
+
+        System.out.println("地图创建测试");
+
+    }
+}
