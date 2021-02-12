a01752473990854a5b99f5df51a30628f8e8da30
hanjiaming <jiamingtom@gmail.com>
2021-02-11 22:23:38 -0400

    commit test

--- src/main/java/MainLoop/MainLoop.java
+++ src/main/java/MainLoop/MainLoop.java
@@ -1,5 +1,11 @@
+package MainLoop;
+
 import java.util.Scanner;
 
+/** The class is to run main activity for the game
+ * @author hanjiaming
+ * @version  1.0.0
+ */
 public class MainLoop {
     public static void main(String[] args){
 
@@ -10,7 +16,7 @@ public class MainLoop {
         String message1 = "Assign the correct number of reinforcement armies to your selected continents according to the Warzone rules. ";
         String message2 = "Issue your aray to targrt continents to attack enemy area or support your own  area, the order will list on the right side";
         String message3 = "To confirm your order on right and execute the order of this round.";
-
+ /*
         System.out.println("      WELCOME TO WARZONE !     ");
         Scanner scan = new Scanner(System.in);
         System.out.println("Do you want load the game from previously saved game (yes/no)?");
@@ -23,10 +29,14 @@ public class MainLoop {
         }
         else
             System.out.println("invalid enter");
+    */
 
-        int player_flag = 1;
-        String winner = null;
-        int i;
+        //Assign Reinforcement phase
+            //调用gameplay里的方法将每回合新增援的兵数分配给每个玩家
+        //Issue Order Phase
+            
+        //Execute Order Phase
+
     }
 }
 
