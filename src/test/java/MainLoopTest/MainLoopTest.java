46ac70142c4253c533156b30df1e5fde21094a32
hanjiaming <jiamingtom@gmail.com>
2021-02-13 02:00:41 -0400

    add JUnit Test

---
+++ src/test/java/MainLoopTest/MainLoopTest.java
@@ -0,0 +1,16 @@
+package MainLoopTest;
+
+import org.junit.Test;
+
+/**
+ * Game engine test unit
+ */
+public class MainLoopTest {
+    @Test
+    /**
+     * this method used to test MainLoop Class
+     */
+    public void testGameEngine() {
+        System.out.println("MainLoop class Tested");
+    }
+}
