84569fcb141bc04ea98c09edf95b13f2d7a7aeea
DeJian Wang <wangdejianwang@gmail.com>
2021-02-20 18:53:56 -0500

    1.GameData class refactoring add multiple data member. 2.GameEngine class refactoring, a lot of changes. 3.Add GameEngineTest.java to test add/remove player

--- src/test/java/gamePlayTest/GameEngineTest.java
+++ src/test/java/gamePlayTest/GameEngineTest.java
@@ -1,5 +1,7 @@
 package gamePlayTest;
 
+import gameplay.GameData;
+import gameplay.GameEngine;
 import org.junit.Test;
 
 /**
@@ -7,11 +9,26 @@ import org.junit.Test;
  */
 
 public class GameEngineTest {
-    @Test
+    GameData d_GameData = new GameData();
+    GameEngine d_GameEngine = new GameEngine(d_GameData);
+
     /**
      * this method used to test GameEngine Class
      */
+    @Test
     public void testGameEngine() {
-        System.out.println("Game Engine Tested");
+        System.out.println("Game Engine Test Start:");
+
+    }
+
+    /**
+     * thie method used to test add and remove player to the game
+     */
+    @Test
+    public void testAddRemovePlayer() {
+        System.out.println("1.check add Player");
+        d_GameEngine.addNewPlayer("red");
+        System.out.println("1.check removed Player");
+        d_GameEngine.RemovePlayer(d_GameEngine.d_GameData.getD_PlayerList().get(0));
     }
 }
