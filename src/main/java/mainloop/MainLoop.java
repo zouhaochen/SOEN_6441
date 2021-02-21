283bd8fdbb191b30b9986d247ab2cb1f0744eb7a
hanjiaming <jiamingtom@gmail.com>
2021-02-21 01:04:50 -0400

    MainLoop framework

--- src/main/java/mainloop/MainLoop.java
+++ src/main/java/mainloop/MainLoop.java
@@ -1,12 +1,9 @@
-package MainLoop;
+package mainloop;
 
+import command.CommandValidator;
 import gameelements.Player;
+import gameplay.GameData;
 import gameplay.GameEngine;
-import gameplay.GamePhase;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 /** The class is to run main activity for the game
@@ -15,56 +12,66 @@ import java.util.Scanner;
  */
 
 public class MainLoop extends GameEngine {
-    private static int total_country;
-    private static int total_player;
-    private static ArrayList <String> country_list;
-    private static ArrayList<String> continent_list;
-    private static String p_colour;
-    private static List<String> countries;
-    private static Map<String, Integer> continent_country_count;
-    private static Map<String, String> country_continent;
-    private static Map<String, Integer> continent_value;
-    private static int total_order;
 
-    private static int gamePhase;
+    private static GameData p_GameData;
+    private static String p_colour;
+    private static CommandValidator d_commandValidator;
+
 
     /**
      * GameEngine Constructor
      *
-     * @param total_country  for import total country
-     * @param total_players  import total players number
-     * @param country_list   country list
-     * @param continent_list continent list
+     * @param p_GameData you should pass reference that Game Data you used
      */
-    public MainLoop(int total_country, int total_players, ArrayList<String> country_list, ArrayList<String> continent_list) {
-        super(total_country, total_players, country_list, continent_list);
+    public MainLoop(GameData p_GameData) {
+        super(p_GameData);
     }
 
-    public static void main(String[] args){
-        Player o_player = new Player(p_colour);
-        GameEngine o_GameEngine = new GameEngine(total_country, total_player,country_list, continent_list);
-        GamePhase o_GamePhase = new GamePhase();
+
+    public static void main(String[] args) {
+        Player o_player = new Player(p_colour, d_commandValidator);
+        GameEngine o_GameEngine = new GameEngine(p_GameData);
+        GameData o_GameData = new GameData();
+        Scanner sc = new Scanner(System.in);
 
         //Assign Reinforcement phase
         //调用gameplay里的方法将每回合新增援的兵数分配给每个玩家
-        Integer RF = o_GameEngine.calReinforcementArmies( countries, continent_country_count, country_continent, continent_value);
-        o_player.setReinforcementArmies(RF);
+        int p_RainfocementArmy = o_player.getReinforcementArmies();
+        o_player.setReinforcementArmies(p_RainfocementArmy);
 
         //Issue Order Phase
         //o_player.issueOrder();
 
         //Execute Order Phase
 
-        //我的任务
-        //获取player的数量和所有player的ID
-
-
         //获取地图信息, 包括所有的country和country的领主信息, 根据规则和player拥有的领土信息分配军队数量
 
         //循环每一轮, 直到有人获胜
-        while( o_GamePhase.getGamePhaseAsInt() != '5') {
+
+        //我的任务
+        System.out.println("Welcome to warzone! ");
+
+        //获取player的数量和所有player的ID
+        System.out.println("Please enter for how many player are going to play: ");
+        int p_playerNum = sc.nextInt();
+        o_GameData.setD_TotalPlayer(p_playerNum);
+
+        System.out.println("Please enter the name for a player: ");
+        for (int i = 0; i < o_GameData.getD_TotalPlayer(); i++) {
+            String p_playerName = sc.nextLine();
+           // o_GameEngine.addNewPlayer(p_playerName);
+        }
+
+        System.out.println("please enter how many country in total: ");
+        int p_countryNum = sc.nextInt();
+        o_GameData.setD_TotalCountry(p_countryNum);
+        System.out.println(o_GameData.getD_TotalCountry() + " contries have already set !");
+
+/*
+        while(o_GameData.getCurrentPhase().getGamePhaseAsInt() == '5' ) {
+
                 //循环遍历所有player, 调用player的issue_order()获取玩家输入的指令并存储, 直到所有玩家都结束下达指令, 将指令存储在Player的orders中
-                for (int i = 0; i < total_order; i++) {
+                for (int i = 0; i < o_GameData.getD_TotalPlayer(); i++) {
                     //我觉得这应该有个 if条件
                     // if（deployReinforcementArmies（p_armyNum）) {
                     //o_player.issueOrder();
@@ -73,12 +80,14 @@ public class MainLoop extends GameEngine {
                 }
 
                 //循环调用player的next_order()获取order, 并调用order的execute()执行命令, 更新所有country上各player的军队信息
-                for (int i = 0; i < total_order; i++) {
+              //  for (int i = 0; i < ; i++) {
                     o_player.nextOrder();
-                }
+             //   }
             //其他指令, 计算本轮结果等
 
         }
+        */
+
     }
 }
 
