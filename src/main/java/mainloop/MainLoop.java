c3b89a1998ea5467c264a803ce380f20fa4a2955
hanjiaming <jiamingtom@gmail.com>
2021-02-21 20:41:51 -0400

    MainLoop framework

--- src/main/java/mainloop/MainLoop.java
+++ src/main/java/mainloop/MainLoop.java
@@ -2,6 +2,7 @@ package mainloop;
 
 import command.CommandValidator;
 import gameelements.Player;
+import gameelements.order.DeployOrder;
 import gameplay.GameData;
 import gameplay.GameEngine;
 import java.util.Scanner;
@@ -30,23 +31,10 @@ public class MainLoop extends GameEngine {
 
     public static void main(String[] args) {
         Player o_player = new Player(p_colour, d_commandValidator);
-        GameEngine o_GameEngine = new GameEngine(p_GameData);
         GameData o_GameData = new GameData();
+        GameEngine o_GameEngine = new GameEngine(o_GameData);
+
         Scanner sc = new Scanner(System.in);
-
-        //Assign Reinforcement phase
-        //调用gameplay里的方法将每回合新增援的兵数分配给每个玩家
-        int p_RainfocementArmy = o_player.getReinforcementArmies();
-        o_player.setReinforcementArmies(p_RainfocementArmy);
-
-        //Issue Order Phase
-        //o_player.issueOrder();
-
-        //Execute Order Phase
-
-        //获取地图信息, 包括所有的country和country的领主信息, 根据规则和player拥有的领土信息分配军队数量
-
-        //循环每一轮, 直到有人获胜
 
         //我的任务
         System.out.println("Welcome to warzone! ");
@@ -56,34 +44,44 @@ public class MainLoop extends GameEngine {
         int p_playerNum = sc.nextInt();
         o_GameData.setTotalPlayer(p_playerNum);
 
-        System.out.println("Please enter the name for a player: ");
+        System.out.println("Please enter the name for each of player: ");
         for (int i = 0; i < o_GameData.getTotalPlayer(); i++) {
-            String p_playerName = sc.nextLine();
-           // o_GameEngine.addNewPlayer(p_playerName);
+            String p_playerName = sc.next();
+            o_GameEngine.addNewPlayer(p_playerName);
         }
 
+        //获取地图信息, 包括所有的country和country的领主信息, 根据规则和player拥有的领土信息分配军队数量
         System.out.println("please enter how many country in total: ");
-        int p_countryNum = sc.nextInt();
-        o_GameData.setTotalCountry(p_countryNum);
+            int p_countryNum = sc.nextInt();
+            o_GameData.setTotalCountry(p_countryNum);
         System.out.println(o_GameData.getTotalCountry() + " contries have already set !");
 
-/*
-        while(o_GameData.getCurrentPhase().getGamePhaseAsInt() == '5' ) {
+
+
+       //游戏机制开始， 循环每一轮, 直到有人获胜
+       while(o_GameData.getCurrentPhase().getGamePhaseAsInt() == '5' ) {
+           //Assign Reinforcement phase 调用gameplay里的方法将每回合新增援的兵数分配给每个玩家
 
                 //循环遍历所有player, 调用player的issue_order()获取玩家输入的指令并存储, 直到所有玩家都结束下达指令, 将指令存储在Player的orders中
-                for (int i = 0; i < o_GameData.getD_TotalPlayer(); i++) {
-                    o_player.issueOrder();
-                }
+                   for (Player l_Player : o_GameData.getPlayerList()) {
+                       l_Player.setReinforcementArmies(5);
 
-                //循环调用player的next_order()获取order, 并调用order的execute()执行命令, 更新所有country上各player的军队信息
-              //  for (int i = 0; i < ; i++) {
-                    o_player.nextOrder();
-             //   }
-            //其他指令, 计算本轮结果等
+                       while(l_Player.getReinforcementArmies()>0) {
+                           l_Player.issueOrder();
+                       }
+                   }
+
+                   for (Player l_Player : o_GameData.getPlayerList()) {
+                        int p_orderList_length = l_Player.getOrdersInCurrentTurn().size();
+
+                       for(int i=0; i< p_orderList_length; i++ ) {
+                           l_Player.nextOrder().execute();
+                       }
+                   }
+
+           //其他指令, 计算本轮结果等
 
         }
-        */
-
     }
 }
 
