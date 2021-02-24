06c94b2c5bfcbfbab8d55802a8d02d48fc2fc95a
NormalZHC <zouhaochenluke@gmail.com>
2021-02-24 21:07:29 +0800

    create MapEdit class junit

---
+++ src/test/java/mapTest/MapEditTest.java
@@ -0,0 +1,50 @@
+package mapTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Auther: Haochen Zou
+ * @version: 1.0
+ */
+public class MapEditTest {
+    @Test
+    public void testEditMap() {
+
+    }
+    @Test
+    public void testGetFile(){
+
+    }
+    @Test
+    public void testEditContinent(){
+
+    }
+    @Test
+    public void testOptType(){
+
+    }
+    @Test
+    public void testTempSave(){
+
+    }
+    @Test
+    public void testEditCountry(){
+
+    }
+    @Test
+    public void testEditNeighbor(){
+
+    }
+    @Test
+    public void testShowMap(){
+
+    }
+    @Test
+    public void testSaveMap(){
+
+    }
+}
