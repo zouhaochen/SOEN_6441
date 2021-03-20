7e702284d6fe1c9d8bdcbb358e4dc2cf83a93c82
WangZT01 <18640096737@163.com>
2021-03-20 23:42:42 +0800

    New State pattern-Zitao Wang.

--- src/main/java/model/state/PostLoad.java
+++ src/main/java/model/state/PostLoad.java
@@ -33,6 +33,19 @@ public class PostLoad extends Edit {
 
 		System.out.println("must save map");
 		d_ml.setPhase(new PlaySetup(d_ml));
+		System.out.println(" =================================================");
+		System.out.println("| #   PHASE                   : command           |");
+		System.out.println(" =================================================");
+		System.out.println("| 1.  Play:PlaySetup:         : load map          |");
+		System.out.println("| 2.  Play:PlaySetup:         : show map          |");
+		System.out.println("| 7.  Play:MainPlay:deploy    : reinforce+deploy  |");
+		System.out.println("| 8.  Play:MainPlay:advance   : advance           |");
+		System.out.println("| 9.  Play:MainPlay:cards     : cards             |");
+		System.out.println("| 10. Play                    : end game          |");
+		System.out.println("| 11. Play:PlaySetup:         : next phase        |");
+		System.out.println("| 12. Any                     : previous phase    |");
+		System.out.println(" =================================================");
+		System.out.println("enter a " + d_ml.getClass().getSimpleName() + " phase command: ");
 	}
 
 	public void previous() {
