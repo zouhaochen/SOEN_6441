1cc4dec54de399a70c5f0fc9abdb78bcf196a5d7
Dejian W <52977418+DeW-Master@users.noreply.github.com>
2021-03-22 18:47:28 -0400

    re-organize all test class

--- src/test/java/modeltest/LogEntryBufferTest.java
+++ src/test/java/modeltest/LogEntryBufferTest.java
@@ -1,3 +1,5 @@
+package modeltest;
+
 import model.LogEntryBuffer;
 import org.junit.After;
 import org.junit.Test;
@@ -7,6 +9,11 @@ public class LogEntryBufferTest {
      * test object
      */
     LogEntryBuffer d_LogEntryBuffer=new LogEntryBuffer();
+
+    /**
+     * test log file path
+     */
+    private String d_FilePath = "log/TestLog.txt";
 
     /**
      * print ok when test is passed
@@ -24,9 +31,9 @@ public class LogEntryBufferTest {
         d_LogEntryBuffer.start();
         System.out.println("test msg1 before first update");
         System.out.println("test msg2 before first update");
-        d_LogEntryBuffer.updateFile("TestLog.txt");
+        d_LogEntryBuffer.updateFile(d_FilePath);
         System.out.println("test msg3 before second update");
-        d_LogEntryBuffer.updateFile("TestLog.txt");
+        d_LogEntryBuffer.updateFile(d_FilePath);
     }
 
 
