3292a5d14d684cecdb1b1ab0ce1c9f275ae2f041
luyongtang <wilsonluyongtang_2005@hotmail.com>
2021-03-24 02:49:53 -0400

    Merge master in and resolve conflicts

--- src/main/java/controller/MainPlayController.java
+++ src/main/java/controller/MainPlayController.java
@@ -5,6 +5,9 @@ import command.CommandValidator;
 import model.GameData;
 import model.LogEntryBuffer;
 import model.Observable;
+import model.gameelements.Continent;
+import model.gameelements.Country;
+import model.gameelements.Player;
 import model.state.Edit;
 import model.state.End;
 import model.state.Phase;
@@ -101,10 +104,10 @@ public class MainPlayController extends Observable {
                 System.out.println("| 2.  Edit                       : savemap           |");
                 System.out.println("| 3.  Play except for LoadMap    : showmap           |");
                 System.out.println("| 4.  Play:Startup:LoadMap       : loadmap           |");
-                System.out.println("| 5.  Play:Startup:AddPlayer     : addPlayers        |");
-                System.out.println("| 6.  Play:MainPlay:IssueOrder   : issueorders       |");
-                System.out.println("| 7.  Play:MainPlay:advance      : advance           |");
-                System.out.println("| 8.  Play:MainPlay:cards        : cards             |");
+                System.out.println("| 5.  Play:Startup:AddPlayer     : addPlayer         |");
+                System.out.println("| 6.  Play:Startup:AssignCountry : assign            |");
+                System.out.println("| 7.  Play:MainPlay:IssueOrder   : issue             |");
+                System.out.println("| 8.  Play:MainPlay:ExecuteOrder : execute           |");
                 System.out.println("| 9. Any                        : end                |");
                 System.out.println(" =====================================================");
                 System.out.println("enter a " + gamePhase.getClass().getSimpleName() + " phase command: ");
@@ -131,14 +134,13 @@ public class MainPlayController extends Observable {
                     case "addplayer":
                         gamePhase.setPlayers();
                         break;
-                    case "issueorders":
+                    case "assign":
+                        gamePhase.assignCountries();
+                    case "issue":
                         gamePhase.issueOrder();
                         break;
-                    case "advance":
+                    case "execute":
                         gamePhase.advance();
-                        break;
-                    case "cards":
-                        gamePhase.cards();
                         break;
                     case "end":
                         gamePhase.endGame();
@@ -151,6 +153,65 @@ public class MainPlayController extends Observable {
 
         } while (mycommand.equals("end"));
 
+    }
+
+    public void issueOrders() {
+        boolean l_Continue;
+        do {
+            l_Continue = false;
+            for (Player l_Player : d_GameData.getPlayerList()) {
+                boolean l_NewOrder = l_Player.issueOrder();
+                l_Continue = l_Continue || l_NewOrder;
+            }
+        } while (l_Continue);
+    }
+
+    public void executeAllOrders() {
+        boolean l_MoreOrder;
+        do {
+            l_MoreOrder = false;
+
+            for (Player l_Player : d_GameData.getPlayerList()) {
+                // one order of the player executes in each turn
+                if (!l_Player.getOrdersInCurrentTurn().isEmpty()) {
+                    l_Player.nextOrder().execute();
+                }
+
+                l_MoreOrder = !l_Player.getOrdersInCurrentTurn().isEmpty() || l_MoreOrder;
+            }
+
+        } while (l_MoreOrder);
+    }
+
+    public void showMap() {
+        System.out.println("\n====================== Map ======================");
+        // Information of continents
+        System.out.println("Continents:");
+        for (Continent l_Continent : d_GameData.getContinentList()) {
+            System.out.println(l_Continent.getName());
+        }
+
+        // Information of countries
+        System.out.println("\nCountries:");
+        for (Country l_Country : d_GameData.getCountryList()) {
+            String l_CountryName = l_Country.getName();
+            int l_Army = l_Country.getArmies();
+            String l_OwnerString;
+            if (l_Country.getOwner() != null) {
+                l_OwnerString = "owned by Player " + l_Country.getOwner().getColour();
+            } else {
+                l_OwnerString = "no owner";
+            }
+            System.out.println(l_CountryName + ": " + l_Army + " armies, " + l_OwnerString);
+            StringBuilder l_StringBuilder = new StringBuilder();
+            for (Country l_Border : l_Country.getBorderCountries().values()) {
+                l_StringBuilder.append(l_Border.getName()).append(", ");
+            }
+            String l_BorderList = l_StringBuilder.delete(l_StringBuilder.length() - 2, l_StringBuilder.length()).toString();
+            System.out.println("\tconnected to: " + l_BorderList);
+        }
+
+        System.out.println("\n=================================================");
     }
 
 }
